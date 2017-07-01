70f2072f875f74ed50131821bdbf608b209f352a
Zaxophone73 <augalde73@hotmail.com>
2017-07-01 02:39:14 -0500

    Original

---
+++ IdiomasPantallas/app/src/main/java/com/ttidtech/idiomaspantallas/MainActivityBienvenida.java
@@ -0,0 +1,13 @@
+package com.ttidtech.idiomaspantallas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityBienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bienvenida);
+    }
+}
